1a537b6c0a4b90d905f78368aa567e02c1c1186b
unknown <luka981223@gmail.com>
2022-04-26 19:31:41 +0400

    first commit

---
+++ homework2/visual studio solution(hw2)/homework 2/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace homework_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine("luka razmadze");
+            var x = Console.ReadLine();
+            Console.WriteLine(x);
+
+
+        }
+    }
+}
